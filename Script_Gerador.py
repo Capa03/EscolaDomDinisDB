2e471b5a5f2ed217f77a5a86185037bfa9b8f704
XarreuJr <vidazinhadiogo@gmail.com>
2023-12-06 00:42:52 +0000

    final2

--- Script_Gerador.py
+++ Script_Gerador.py
@@ -4,89 +4,89 @@ import pyodbc
 fake = Faker()
 
 # Configuração da conexão com o banco de dados SQL Server
-conn_str = (
-    r'Driver=SQL Server;'
-    r'Server=.\SQLEXPRESS;'
-    r'Database=EscolaDomDinis;'
-    r'Trusted_Connection=yes;'
-    )
-conexao = pyodbc.connect(conn_str)
+conexao_str = 'DRIVER={SQL Server};SERVER=servidor_sql;DATABASE=EscolaDomDinis;UID=usuario;PWD=senha'
+conexao = pyodbc.connect(conexao_str)
 
 # Criação de um cursor
 cursor = conexao.cursor()
 
-# Defina o número de alunos que você deseja gerar
-num_alunos = 1000
+# Defina o número de registros que você deseja gerar para cada tabela
+num_alunos = 100
+num_cursos = 10
+num_turmas = 50
+num_disciplinas = 20
+num_docentes = 20
+num_questionarios = 50
+num_respostas = 200
+num_avaliacoes = 150
 
-# Insira dados aleatórios na tabela Aluno
-for _ in range(num_alunos):
-    numero_turma = fake.random_int(min=1, max=10)  # ajuste conforme necessário
-    nome_aluno = fake.name()
-    email = fake.email()
-    contato = fake.phone_number()
+# Insira dados aleatórios na tabela Curso
+for _ in range(num_cursos):
+    nome_curso = fake.job()
+    numero_edicao = fake.random_int(min=1, max=5)  # ajuste conforme necessário
 
-    # Query de inserção
-    query = f"INSERT INTO Aluno (Numero_turma, Nome_aluno, Email, Contato) VALUES ({numero_turma}, '{nome_aluno}', '{email}', '{contato}')"
-
-    # Execute a query
+    query = f"INSERT INTO Curso (Nome, Numero_edicao) VALUES ('{nome_curso}', {numero_edicao})"
     cursor.execute(query)
 
-# Defina o número de docentes que você deseja gerar
-num_docentes = 200
+# Insira dados aleatórios na tabela Turma
+for _ in range(num_turmas):
+    numero_curso_turma = fake.random_int(min=1, max=num_cursos)
+    
+    query = f"INSERT INTO Turma (Numero_curso) VALUES ({numero_curso_turma})"
+    cursor.execute(query)
+
+# Insira dados aleatórios na tabela Disciplina
+for _ in range(num_disciplinas):
+    nome_disciplina = fake.word()
+    numero_curso_disciplina = fake.random_int(min=1, max=num_cursos)
+    
+    query = f"INSERT INTO Disciplina (Nome_disciplina, Numero_curso) VALUES ('{nome_disciplina}', {numero_curso_disciplina})"
+    cursor.execute(query)
 
 # Insira dados aleatórios na tabela Docente
 for _ in range(num_docentes):
     nome_docente = fake.name()
-    endereco = fake.address()
+    endereco_docente = fake.address()
 
-    # Query de inserção
-    query = f"INSERT INTO Docente (Nome_docente, Endereço) VALUES ('{nome_docente}', '{endereco}')"
-
-    # Execute a query
+    query = f"INSERT INTO Docente (Nome_docente, Endereço) VALUES ('{nome_docente}', '{endereco_docente}')"
     cursor.execute(query)
-    
-num_questionarios = 500
+
+# Insira dados aleatórios na tabela Aluno
+for _ in range(num_alunos):
+    numero_turma_aluno = fake.random_int(min=1, max=num_turmas)
+    nome_aluno = fake.name()
+    email_aluno = fake.email()
+    contato_aluno = fake.phone_number()
+
+    query = f"INSERT INTO Aluno (Numero_turma, Nome_aluno, Email, Contato) VALUES ({numero_turma_aluno}, '{nome_aluno}', '{email_aluno}', '{contato_aluno}')"
+    cursor.execute(query)
 
 # Insira dados aleatórios na tabela Questionario
 for _ in range(num_questionarios):
-    numero_pergunta = fake.random_int(min=1, max=10)  # ajuste conforme necessário
+    numero_pergunta = fake.random_int(min=1, max=10)
     perguntas = fake.text(max_nb_chars=255)
-    numero_disciplina = fake.random_int(min=1, max=10)  # ajuste conforme necessário
-    numero_docente = fake.random_int(min=1, max=20)  # ajuste conforme necessário
+    numero_disciplina_questionario = fake.random_int(min=1, max=num_disciplinas)
+    numero_docente_questionario = fake.random_int(min=1, max=num_docentes)
 
-    # Query de inserção
-    query = f"INSERT INTO Questionario (Numero_pergunta, Perguntas, Numero_disciplina, Numero_docente) VALUES ({numero_pergunta}, '{perguntas}', {numero_disciplina}, {numero_docente})"
-
-    # Execute a query
+    query = f"INSERT INTO Questionario (Numero_pergunta, Perguntas, Numero_disciplina, Numero_docente) VALUES ({numero_pergunta}, '{perguntas}', {numero_disciplina_questionario}, {numero_docente_questionario})"
     cursor.execute(query)
-    
-num_respostas = 500
 
 # Insira dados aleatórios na tabela Resposta
 for _ in range(num_respostas):
     texto_resposta = fake.text(max_nb_chars=255)
-    numero_aluno = fake.random_int(min=1, max=100)  # ajuste conforme necessário
-    numero_questionario = fake.random_int(min=1, max=50)  # ajuste conforme necessário
+    numero_aluno_resposta = fake.random_int(min=1, max=num_alunos)
+    numero_questionario_resposta = fake.random_int(min=1, max=num_questionarios)
 
-    # Query de inserção
-    query = f"INSERT INTO Resposta (Texto, Numero_aluno, Numero_questionario) VALUES ('{texto_resposta}', {numero_aluno}, {numero_questionario})"
-
-    # Execute a query
+    query = f"INSERT INTO Resposta (Texto, Numero_aluno, Numero_questionario) VALUES ('{texto_resposta}', {numero_aluno_resposta}, {numero_questionario_resposta})"
     cursor.execute(query)
-    
-# Defina o número de avaliações que você deseja gerar
-num_avaliacoes = 1000
 
 # Insira dados aleatórios na tabela Avaliacao
 for _ in range(num_avaliacoes):
-    numero_aluno_avaliacao = fake.random_int(min=1, max=100)  # ajuste conforme necessário
-    numero_disciplina_avaliacao = fake.random_int(min=1, max=10)  # ajuste conforme necessário
-    nota_avaliacao = fake.random_int(min=1, max=10)  # ajuste conforme necessário
+    numero_aluno_avaliacao = fake.random_int(min=1, max=num_alunos)
+    numero_disciplina_avaliacao = fake.random_int(min=1, max=num_disciplinas)
+    nota_avaliacao = fake.random_int(min=1, max=10)
 
-    # Query de inserção
     query = f"INSERT INTO Avaliacao (Numero_aluno, Numero_disciplina, Nota_avaliacao) VALUES ({numero_aluno_avaliacao}, {numero_disciplina_avaliacao}, {nota_avaliacao})"
-
-    # Execute a query
     cursor.execute(query)
 
 # Commit das alterações
@@ -95,4 +95,4 @@ conexao.commit()
 # Feche a conexão
 conexao.close()
 
-print(f'Dados gerados e inseridos na tabela Aluno.')
+print('Dados gerados e inseridos em todas as tabelas.')
