675b51d05a4c7d37d2b9e6144f591dda4b056fb2
David Capa <capaseitadavid@gmail.com>
2023-12-09 02:14:01 +0000

    Refactor table names and column names in CreateQuery.sql

--- CreateQuery.sql
+++ CreateQuery.sql
@@ -2,65 +2,59 @@
 USE EscolaDomDinis;
 go 
 
-CREATE TABLE Aluno (
- Numero_aluno INT PRIMARY KEY IDENTITY(1,1),
- Numero_turma INT,
- Nome_aluno VARCHAR(100),
- Email VARCHAR(100),
- Contato VARCHAR(20)
-);
-
 CREATE TABLE Curso (
- Numero_curso INT PRIMARY KEY IDENTITY(1,1),
- Nome VARCHAR(100),
- Numero_edicao INT
+ idCurso INT PRIMARY KEY IDENTITY(1,1),
+ nome VARCHAR(100),
+ edicao INT
 );
 
 CREATE TABLE Turma (
- Numero_turma INT PRIMARY KEY IDENTITY(1,1),
- Numero_curso INT,
- FOREIGN KEY (Numero_curso) REFERENCES Curso(Numero_curso)
+ idTurma INT PRIMARY KEY IDENTITY(1,1),
+ idCurso INT,
+ FOREIGN KEY (idCurso) REFERENCES Curso(idCurso)
 );
 
 CREATE TABLE Disciplina (
- Numero_disciplina INT PRIMARY KEY IDENTITY(1,1),
- Nome_disciplina VARCHAR(100),
- Numero_curso INT,
- FOREIGN KEY (Numero_curso) REFERENCES Curso(Numero_curso)
+ idDisciplina INT PRIMARY KEY IDENTITY(1,1),
+ nome VARCHAR(100),
+ idCurso INT,
+ FOREIGN KEY (idCurso) REFERENCES Curso(idCurso)
 );
 
-CREATE TABLE Docente (
- Numero_docente INT PRIMARY KEY IDENTITY(1,1),
- Nome_docente VARCHAR(100),
- Endereço VARCHAR(100)
+CREATE TABLE Aluno (
+ idAluno INT PRIMARY KEY IDENTITY(1,1),
+ idTurma INT,
+ nome VARCHAR(100),
+ email VARCHAR(100),
+ contacto VARCHAR(20)
+ FOREIGN KEY (idTurma) REFERENCES Turma(idTurma)
 );
 
 
 CREATE TABLE Questionario (
- Numero_questionario INT PRIMARY KEY IDENTITY(1,1),
- Numero_pergunta INT,
- Perguntas VARCHAR(255),
- Numero_disciplina INT,
- Numero_docente INT,
- FOREIGN KEY (Numero_disciplina) REFERENCES Disciplina(Numero_disciplina),
- FOREIGN KEY (Numero_docente) REFERENCES Docente(Numero_docente)
+ idQuestionario INT PRIMARY KEY IDENTITY(1,1),
+ pergunta VARCHAR(255),
+ idDisciplina INT,
+ idDocente INT,
+ FOREIGN KEY (idDisciplina) REFERENCES Disciplina(idDisciplina),
+ FOREIGN KEY (idDocente) REFERENCES Docente(idDocente)
 );
 
 CREATE TABLE Resposta (
- Numero_resposta INT PRIMARY KEY IDENTITY(1,1),
- Texto VARCHAR(255),
- Numero_aluno INT,
- Numero_questionario INT,
- FOREIGN KEY (Numero_aluno) REFERENCES Aluno(Numero_aluno),
- FOREIGN KEY (Numero_questionario) REFERENCES Questionario(Numero_questionario)
+ idResposta INT PRIMARY KEY IDENTITY(1,1),
+ texto VARCHAR(255),
+ idAluno INT,
+ idQuestionario INT,
+ FOREIGN KEY (idAluno) REFERENCES Aluno(idAluno),
+ FOREIGN KEY (idQuestionario) REFERENCES Questionario(idQuestionario)
 );
 
 CREATE TABLE Avaliacao (
- Numero_avaliacao INT PRIMARY KEY IDENTITY(1,1),
- Numero_aluno INT,
- Numero_disciplina INT,
- Nota_avaliacao INT,
- FOREIGN KEY (Numero_aluno) REFERENCES Aluno(Numero_aluno),
- FOREIGN KEY (Numero_disciplina) REFERENCES Disciplina(Numero_disciplina)
+ idAvaliacao INT PRIMARY KEY IDENTITY(1,1),
+ idAluno INT,
+ idDisciplina INT,
+ nota INT,
+ FOREIGN KEY (idAluno) REFERENCES Aluno(idAluno),
+ FOREIGN KEY (idDisciplina) REFERENCES Disciplina(idDisciplina)
 );
 
---
+++ Script_Gerador.py
@@ -0,0 +1,204 @@
+from faker import Faker
+import pyodbc
+
+fake = Faker('pt_PT')
+
+# Configuração da conexão com o banco de dados SQL Server
+conexao_str = (
+    r'Driver={ODBC Driver 17 for SQL Server};'
+    r'Server=CAPA;'
+    r'Database=EscolaDomDinis;'
+    r'Trusted_Connection=yes;'
+)
+conexao = pyodbc.connect(conexao_str)
+
+# Criação de um cursor
+cursor = conexao.cursor()
+
+# Número de registros a serem inseridos
+numAlunos = 1000
+numProfessores = 10
+numCursos = 100
+numDisciplinas = 50
+numInscricoes = 1000
+numAvaliacoes = 1000
+numTurmas = 10
+numDocentes = 10
+numQuestionarios = 1000
+numRespostas = 1000
+
+# Inserção de dados na tabela Curso
+for _ in range(numCursos):
+    try:
+        nome = fake.name()
+        
+        query = f"INSERT INTO Curso (nome) VALUES ('{nome}')"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Curso: {e}")
+
+# Inserção de dados na tabela Turma
+for _ in range(numTurmas):
+    try:
+        idCurso = fake.random_int(min=1, max=numCursos)
+        query = f"INSERT INTO Turma (idCurso) VALUES ({idCurso})"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Turma: {e}")
+
+# Inserção de dados na tabela Disciplina
+for _ in range(numDisciplinas):
+    try:
+        nome = fake.name()
+
+        # Generate a valid idCurso value
+        idCurso = fake.random_int(min=1, max=numCursos)
+
+        # Ensure that the generated idCurso exists in the Curso table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Curso WHERE idCurso = {idCurso}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idCurso doesn't exist, generate a new one
+                idCurso = fake.random_int(min=1, max=numCursos)
+
+        query = f"INSERT INTO Disciplina (nome, idCurso) VALUES ('{nome}', {idCurso})"
+        cursor.execute(query)
+        conexao.commit()
+        print(f"Disciplina inserted: {nome}")
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Disciplina: {e}")
+
+
+# Inserção de dados na tabela Aluno
+for _ in range(numAlunos):
+    try:
+        idTurma = fake.random_int(min=1, max=numTurmas)
+        
+        # Ensure that the generated idTurma exists in the Turma table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Turma WHERE idTurma = {idTurma}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idTurma doesn't exist, generate a new one
+                idTurma = fake.random_int(min=1, max=numTurmas)
+
+        nome = fake.name()
+        contacto = fake.random_int(min=100000000, max=999999999)
+        email = f"{nome.lower().replace(' ', '_')}@stu.ipbeja.com"
+        query = f"INSERT INTO Aluno (idTurma, nome, contacto, email) VALUES ({idTurma}, '{nome}', '{contacto}', '{email}')"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Aluno: {e}")
+
+
+# Inserção de dados na tabela Docente
+for _ in range(numDocentes):
+    try:
+        nome = fake.name()
+        morada = fake.address()
+        query = f"INSERT INTO Docente (nome, morada) VALUES ('{nome}', '{morada}')"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Docente: {e}")
+
+# Inserção de dados na tabela Questionario
+for _ in range(numQuestionarios):
+    try:
+        pergunta = fake.word()[0:50]
+
+        # Generate a valid idDisciplina value
+        idDisciplina = fake.random_int(min=1, max=numDisciplinas)
+
+        # Ensure that the generated idDisciplina exists in the Disciplina table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Disciplina WHERE idDisciplina = {idDisciplina}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idDisciplina doesn't exist, generate a new one
+                idDisciplina = fake.random_int(min=1, max=numDisciplinas)
+
+        query = f"INSERT INTO Questionario (pergunta, idDisciplina) VALUES ('{pergunta}', {idDisciplina})"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Questionario: {e}")
+
+
+# Inserção de dados na tabela Resposta
+for _ in range(numRespostas):
+    try:
+        idQuestionario = fake.random_int(min=1, max=numQuestionarios)
+
+        # Ensure that the generated idQuestionario exists in the Questionario table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Questionario WHERE idQuestionario = {idQuestionario}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idQuestionario doesn't exist, generate a new one
+                idQuestionario = fake.random_int(min=1, max=numQuestionarios)
+
+        idAluno = fake.random_int(min=1, max=numAlunos)
+
+        # Ensure that the generated idAluno exists in the Aluno table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Aluno WHERE idAluno = {idAluno}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idAluno doesn't exist, generate a new one
+                idAluno = fake.random_int(min=1, max=numAlunos)
+
+        texto = fake.random_int(min=0, max=5)
+        query = f"INSERT INTO Resposta (texto, idAluno, idQuestionario) VALUES ('{texto}', {idAluno}, {idQuestionario})"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Resposta: {e}")
+
+
+# Inserção de dados na tabela Avaliacao
+for _ in range(numAvaliacoes):
+    try:
+        idAluno = fake.random_int(min=1, max=numAlunos)
+
+        # Ensure that the generated idAluno exists in the Aluno table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Aluno WHERE idAluno = {idAluno}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idAluno doesn't exist, generate a new one
+                idAluno = fake.random_int(min=1, max=numAlunos)
+
+        # Generate a valid idDisciplina value
+        idDisciplina = fake.random_int(min=1, max=numDisciplinas)
+
+        # Ensure that the generated idDisciplina exists in the Disciplina table
+        while True:
+            cursor.execute(f"SELECT 1 FROM Disciplina WHERE idDisciplina = {idDisciplina}")
+            if cursor.fetchone():
+                break
+            else:
+                # If idDisciplina doesn't exist, generate a new one
+                idDisciplina = fake.random_int(min=1, max=numDisciplinas)
+
+        nota = fake.random_int(min=0, max=20)
+        query = f"INSERT INTO Avaliacao (idAluno, idDisciplina, nota) VALUES ({idAluno}, {idDisciplina}, {nota})"
+        cursor.execute(query)
+        conexao.commit()
+    except pyodbc.Error as e:
+        print(f"Error inserting record into Avaliacao: {e}")
+
+# Feche a conexão
+conexao.close()
+
+print('Dados gerados e inseridos em todas as tabelas.')
