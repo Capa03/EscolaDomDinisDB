aee89697df8d9404a10a0d98e012bb32104f98ff
David Capa <capaseitadavid@gmail.com>
2023-12-26 19:25:56 +0000

    Refactor stored procedures and add triggers

--- StoredProcedureQuery.sql
+++ StoredProcedureQuery.sql
@@ -1,16 +1,20 @@
 
 
-CREATE PROCEDURE usp_InsertAluno
+CREATE PROCEDURE InsertAluno
     @Nome VARCHAR(100),
     @Contacto VARCHAR(20),
     @Email VARCHAR(100),
     @IdTurma INT
 AS
 BEGIN
-    IF EXISTS (SELECT 1 FROM Turma WHERE IdTurma = @IdTurma)
+    IF EXISTS (SELECT 1
+    FROM Turma
+    WHERE IdTurma = @IdTurma)
     BEGIN
-        INSERT INTO Aluno (Nome, Contacto, Email, IdTurma)
-        VALUES (@Nome, @Contacto, @Email, @IdTurma);
+        INSERT INTO Aluno
+            (Nome, Contacto, Email, IdTurma)
+        VALUES
+            (@Nome, @Contacto, @Email, @IdTurma);
         PRINT 'Aluno inserido com sucesso!';
     END
     ELSE
@@ -19,6 +23,118 @@ BEGIN
     END
 END;
 
+
+CREATE PROCEDURE SPInsertTurma
+    @idCurso INT,
+AS
+BEGIN
+    IF EXISTS (SELECT 1
+    FROM Escola.Curso
+    WHERE idCurso = @idCurso)
+    BEGIN
+        INSERT INTO Escola.Turma
+            (idCurso)
+        VALUES
+            (@idCurso);
+        PRINT 'Turma inserido com sucesso!';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Erro: A Curso especificado não existe!';
+    END
+END;
+
+
+CREATE PROCEDURE SPInsertDisciplina
+    @Nome VARCHAR(50),
+    @idCurso INT
+AS
+BEGIN
+    IF EXISTS (SELECT 1
+    FROM Escola.Curso
+    WHERE idCurso = @idCurso)
+    BEGIN
+        INSERT INTO Escola.Disciplina
+            (Nome, idCurso)
+        VALUES
+            (@Nome, @idCurso);
+        PRINT 'Disciplina inserida com sucesso!';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Erro: A Curso especificado não existe!';
+    END
+END;
+
+
+CREATE PROCEDURE SPQuestionario
+    @Pergunta VARCHAR(255),
+    @idDisciplina INT,
+    @idDocente INT
+AS
+BEGIN
+    IF EXISTS (SELECT 1
+    FROM Escola.Disciplina
+    WHERE idDisciplina = @idDisciplina)
+    BEGIN
+        IF EXISTS (SELECT 1
+        FROM Escola.Docente
+        WHERE idDocente = @idDocente)
+        BEGIN
+            INSERT INTO Escola.Questionario
+                (Pergunta, idDisciplina, idDocente)
+            VALUES
+                (@Pergunta, @idDisciplina, @idDocente);
+            PRINT 'Questionario inserido com sucesso!';
+        END
+        ELSE
+        BEGIN
+            PRINT 'Erro: O Docente especificado não existe!';
+        END
+    END
+    ELSE
+    BEGIN
+        PRINT 'Erro: A Disciplina especificada não existe!';
+    END
+END;
+
+CREATE PROCEDURE SPAvaliacao
+    @idAluno INT,
+    @idDisciplina INT,
+    @Nota INT
+AS
+BEGIN
+    IF EXISTS (SELECT 1
+    FROM Escola.Aluno
+    WHERE idAluno = @idAluno)
+    BEGIN
+        IF EXISTS (SELECT 1
+        FROM Escola.Disciplina
+        WHERE idDisciplina = @idDisciplina)
+        BEGIN
+            IF @Nota >= 0 AND @Nota <= 20
+            BEGIN
+                INSERT INTO Escola.Avaliacao
+                    (idAluno, idDisciplina, Nota)
+                VALUES
+                    (@idAluno, @idDisciplina, @Nota);
+                PRINT 'Avaliacao inserida com sucesso!';
+            END
+            ELSE
+            BEGIN
+                PRINT 'Erro: A nota deve estar no intervalo de 0 a 20!';
+            END
+        END
+        ELSE
+        BEGIN
+            PRINT 'Erro: A Disciplina especificada não existe!';
+        END
+    END
+    ELSE
+    BEGIN
+        PRINT 'Erro: O Aluno especificado não existe!';
+    END
+END;
 
 CREATE TRIGGER tr_CheckNota
 ON Avaliacao
@@ -33,7 +149,8 @@ BEGIN
     IF @Nota < 0 OR @Nota > 20
     BEGIN
         PRINT 'Erro: A nota deve estar no intervalo de 0 a 20!';
-        ROLLBACK; -- Desfaz a transação devido a um erro
+        ROLLBACK;
+    -- Desfaz a transação devido a um erro
     END
     ELSE
     BEGIN
