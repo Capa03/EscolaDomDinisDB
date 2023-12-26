6edcd3662c09c84db293fc5c9e2888a71ef7a780
XarreuJr <vidazinhadiogo@gmail.com>
2023-12-26 22:00:35 +0000

    hello

--- Triggers.sql
+++ Triggers.sql
@@ -1,11 +1,19 @@
--- Criação do trigger
-CREATE TRIGGER Escola.trg_AfterInsertAluno
+CREATE TRIGGER Escola.trg_AfterUpdateAluno
 ON Escola.Aluno
-AFTER INSERT
+AFTER UPDATE
 AS
 BEGIN
-    -- Inserir informações na tabela de histórico
-    INSERT INTO Escola.HistoricoInsercaoAluno (Nome, Contacto, Email, IdTurma, DataInsercao)
-    SELECT Nome, Contacto, Email, IdTurma, GETDATE()
-    FROM inserted;
+    IF UPDATE(IdTurma) -- Verifica se a coluna IdTurma foi atualizada
+    BEGIN
+        -- Inserir informações na tabela de histórico
+        INSERT INTO Escola.HistoricoMudancaTurma (IdAluno, IdTurmaAntiga, IdTurmaNova, DataMudanca)
+        SELECT 
+            i.IdAluno, 
+            d.IdTurma AS IdTurmaAntiga, 
+            i.IdTurma AS IdTurmaNova, 
+            GETDATE()
+        FROM inserted i
+        INNER JOIN deleted d ON i.IdAluno = d.IdAluno
+        WHERE i.IdTurma <> d.IdTurma; -- Inserir apenas se o IdTurma foi realmente alterado
+    END
 END;
